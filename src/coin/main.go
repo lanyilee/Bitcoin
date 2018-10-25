935f4ddc1162315b02a8b819621169c095b4e223
lanyi <lanyilee@qq.com>
2018-10-25 16:24:34 +0800

    main_test

--- src/coin/main.go
+++ src/coin/main.go
@@ -1,14 +1,17 @@
 package main
 
-import {
+import (
 	"core"
-}
+	"fmt"
+)
 
-
-
-
-
-
-func main(){
+func main() {
 	bc := core.NewBlockChain()
+	bc.AddBlock("a send 1 btc to b")
+	bc.AddBlock("b earn 20 btc")
+	for _, block := range bc.Blocks {
+		fmt.Printf("prevHash:%d\n", block.PrevBlockHash)
+		fmt.Printf("data:%s\n", block.Data)
+		fmt.Printf("hash:%d\n", block.Hash)
+	}
 }
